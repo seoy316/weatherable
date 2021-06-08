b3f05f2bc8658442858bf2a6bac19d4568e75b15
seoy316 <67985667+seoy316@users.noreply.github.com>
2021-06-09 03:09:54 +0900

    email 데이터베이스 저장

--- app/src/main/java/kr/ac/kumoh/s/weatherable/SignInActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s/weatherable/SignInActivity.kt
@@ -10,18 +10,29 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.android.volley.AuthFailureError
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
+import org.json.JSONException
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
 
+    companion object {
+        var requestQueue: RequestQueue? = null
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        requestQueue = Volley.newRequestQueue(applicationContext)
 
         auth = FirebaseAuth.getInstance()
         db = FirebaseFirestore.getInstance()
@@ -59,6 +70,8 @@ class SignInActivity : AppCompatActivity() {
                 if (it.isSuccessful) {
                     Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
                     val user = auth.currentUser
+                    print("email: $email")
+                    postUid(email)
                     updateUI(user)
 
                     val intent = Intent(this, LogInActivity::class.java)
@@ -94,6 +107,32 @@ class SignInActivity : AppCompatActivity() {
                 .addOnFailureListener {
                 }
         }
+    }
+
+    fun postUid(user: String) {
+        val url = TourListViewModel.SERVER_URL + "user"
+        val request: StringRequest = object : StringRequest(
+            Method.POST, url,
+            Response.Listener { response ->
+                try {
+                    println("연결 성공")
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+            },
+            Response.ErrorListener { }) {
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String>
+            {
+                val params = HashMap<String, String>()
+                params.put("uid",user)
+                print("params $params")
+                return params
+            }
+        }
+        request.setShouldCache(false)
+        requestQueue?.add(request)
     }
 
     private fun getEmail(user: FirebaseUser?): String { return user!!.email.toString() }
