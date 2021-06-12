e1b348babb2dd38e2951ae947ea5716b8bd8a96c
20171145 <20171145@kumoh.ac.kr>
2021-06-12 22:19:56 +0900

    Update SurveyRainyActivity.kt
    Update SurveyRainyActivity

--- app/src/main/java/kr/ac/kumoh/s/weatherable/SurveyRainyActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s/weatherable/SurveyRainyActivity.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RadioButton
-import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -24,9 +23,13 @@ class SurveyRainyActivity : AppCompatActivity() {
     private lateinit var mSurveyRainyModel: SurveyRainyViewModel
     private val mSurveyRainyAdapter = SurveyRainyAdapter()
     private lateinit var getData: GetData
+    val setData = SetData()
 
     private var uid: Int? = null
+    private var tid: Int? = null
     private lateinit var email: String
+    private lateinit var name: String
+    private val tid_list = arrayListOf<Int>()
 
     companion object {
         var requestQueue: RequestQueue? = null
@@ -59,6 +62,11 @@ class SurveyRainyActivity : AppCompatActivity() {
 
         val btn_survey_rainy = findViewById<Button>(R.id.btn_survey_rainy)
         btn_survey_rainy.setOnClickListener {
+//            for(i in 0 until mSurveyRainyModel.getSize()) {
+//                setData.postRate(uid, tid_list[i], 3, rating[i])
+//            }
+
+            setData.postRate(uid, tid_list, 3, rating)
             val sunny = Intent(this, SurveySunnyActivity::class.java)
             startActivity(sunny)
         }
@@ -67,11 +75,20 @@ class SurveyRainyActivity : AppCompatActivity() {
     inner class SurveyRainyAdapter: RecyclerView.Adapter<SurveyRainyAdapter.ViewHolder>() {
         inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)  {
             val txName: TextView
-            val rgRainy: RadioGroup
+            val rgItem1: RadioButton
+            val rgItem2: RadioButton
+            val rgItem3: RadioButton
+            val rgItem4: RadioButton
+            val rgItem5: RadioButton
 
             init {
                 txName = itemView.findViewById<TextView>(R.id.tx_survey_rainy_name)
-                rgRainy = itemView.findViewById<RadioGroup>(R.id.rg_survey_rainy)
+                rgItem1 = itemView.findViewById<RadioButton>(R.id.radio_item1)
+                rgItem2 = itemView.findViewById<RadioButton>(R.id.radio_item2)
+                rgItem3 = itemView.findViewById<RadioButton>(R.id.radio_item3)
+                rgItem4 = itemView.findViewById<RadioButton>(R.id.radio_item4)
+                rgItem5 = itemView.findViewById<RadioButton>(R.id.radio_item5)
+//                rgGroup.checkedRadioButtonId
             }
         }
 
@@ -89,70 +106,100 @@ class SurveyRainyActivity : AppCompatActivity() {
 
         override fun onBindViewHolder(holder: SurveyRainyAdapter.ViewHolder, position: Int) {
             holder.txName.text = mSurveyRainyModel.getSurveyRainy(position).name
-        }
 
-    }
+            tid_list.add(mSurveyRainyModel.getSurveyRainy(position).id)
+            Log.i("d", "tid_list$tid_list")
 
-    fun onRadioButtonClicked(view: View) {
-        if (view is RadioButton) {
-            // Is the button now checked?
-            val checked = view.isChecked
+            holder.rgItem1.setOnClickListener {
+                rating[position] = 1
+                setRadio(holder, 1)
+//                email = intent.getStringExtra("email").toString()
+                email = "20171145@kumoh.ac.kr"
+                for (i in 0 until getData.getUserSize()) {
+                    if (email == getData.getUser(i).email) {
+                        uid = getData.getUser(i).id
+                        Log.i("d", "userID$uid")
+                    }
+                }
+                Log.i("d", "RatingChecked$rating")
+            }
 
-            // Check which radio button was clicked
-            when (view.getId()) {
-                R.id.radio_item1 ->
-                    if (checked) {
-                        for(i in 0 until getData.getUserSize()) {
-                            email = intent.getStringExtra("email").toString()
-//                            email = "20171145@kumoh.ac.kr"
-                            if (email == getData.getUser(i).email) {
-                                uid = getData.getUser(i).id
-                            }
-                        }
-                        Log.i("d", "userid$uid")
+            holder.rgItem2.setOnClickListener {
+                rating[position] = 2
+                setRadio(holder, 2)
+//                email = intent.getStringExtra("email").toString()
+                email = "20171145@kumoh.ac.kr"
+                for (i in 0 until getData.getUserSize()) {
+                    if (email == getData.getUser(i).email) {
+                        uid = getData.getUser(i).id
+                        Log.i("d", "userID$uid")
                     }
-                R.id.radio_item2 ->
-                    if (checked) {
-                        for(i in 0 until getData.getUserSize()) {
-                            email = intent.getStringExtra("email").toString()
-                            if (email == getData.getUser(i).email) {
-                                uid = getData.getUser(i).id
-                            }
-                        }
-                        Log.i("d", "userid$uid")
+                }
+                Log.i("d", "RatingChecked$rating")
+            }
+
+            holder.rgItem3.setOnClickListener {
+                rating[position] = 3
+                setRadio(holder, 3)
+//                email = intent.getStringExtra("email").toString()
+                email = "20171145@kumoh.ac.kr"
+                for (i in 0 until getData.getUserSize()) {
+                    if (email == getData.getUser(i).email) {
+                        uid = getData.getUser(i).id
+                        Log.i("d", "userID$uid")
                     }
-                R.id.radio_item3 ->
-                    if (checked) {
-                        for(i in 0 until getData.getUserSize()) {
-                            email = intent.getStringExtra("email").toString()
-                            if (email == getData.getUser(i).email) {
-                                uid = getData.getUser(i).id
-                            }
-                        }
-                        Log.i("d", "userid$uid")
+                }
+                Log.i("d", "RatingChecked$rating")
+            }
+
+            holder.rgItem4.setOnClickListener {
+                rating[position] = 4
+                setRadio(holder, 4)
+//                email = intent.getStringExtra("email").toString()
+                email = "20171145@kumoh.ac.kr"
+                for (i in 0 until getData.getUserSize()) {
+                    if (email == getData.getUser(i).email) {
+                        uid = getData.getUser(i).id
+                        Log.i("d", "userID$uid")
                     }
-                R.id.radio_item4 ->
-                    if (checked) {
-                        for(i in 0 until getData.getUserSize()) {
-                            email = intent.getStringExtra("email").toString()
-                            if (email == getData.getUser(i).email) {
-                                uid = getData.getUser(i).id
-                            }
-                        }
-                        Log.i("d", "userid$uid")
+                }
+                Log.i("d", "RatingChecked$rating")
+            }
+
+            holder.rgItem5.setOnClickListener {
+                rating[position] = 5
+                setRadio(holder, 5)
+//                email = intent.getStringExtra("email").toString()
+                email = "20171145@kumoh.ac.kr"
+                for (i in 0 until getData.getUserSize()) {
+                    if (email == getData.getUser(i).email) {
+                        uid = getData.getUser(i).id
+                        Log.i("d", "userID$uid")
                     }
-                R.id.radio_item5 ->
-                    if (checked) {
-                        for(i in 0 until getData.getUserSize()) {
-//                                    email = intent. getStringExtra("email").toString()
-                            if (getData.getUser(i).email == email) {
-                                uid = getData.getUser(i).id
-                                Log.i("d", "userid$uid")
-                            }
-                        }
-//                                Log.i("d", "userid$uid")
-                    }
+                }
+                Log.i("d", "RatingChecked$rating")
             }
         }
+
+        private fun setRadio(holder: SurveyRainyAdapter.ViewHolder, selection: Int) {
+            holder.rgItem1.isChecked = false
+            holder.rgItem2.isChecked = false
+            holder.rgItem3.isChecked = false
+            holder.rgItem4.isChecked = false
+            holder.rgItem5.isChecked = false
+
+//            rb1.isChecked = false
+//            rb2.isChecked = false
+//            rb3.isChecked = false
+//            rb4.isChecked = false
+//            rb5.isChecked = false
+
+            if (selection == 1) holder.rgItem1.isChecked = true
+            if (selection == 2) holder.rgItem2.isChecked = true
+            if (selection == 3) holder.rgItem3.isChecked = true
+            if (selection == 4) holder.rgItem4.isChecked = true
+            if (selection == 5) holder.rgItem5.isChecked = true
+        }
+
     }
 }
