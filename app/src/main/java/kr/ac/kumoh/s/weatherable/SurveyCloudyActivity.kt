67ec2103c90fc6fbf1eac4e79bd5bf46f9d131ae
20171145 <20171145@kumoh.ac.kr>
2021-06-19 20:01:23 +0900

    re

--- app/src/main/java/kr/ac/kumoh/s/weatherable/SurveyCloudyActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s/weatherable/SurveyCloudyActivity.kt
@@ -16,19 +16,16 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.Volley
-import kotlinx.android.synthetic.main.activity_survey_rainy.*
+import kotlinx.android.synthetic.main.activity_survey_cloudy.*
 
 
-class SurveyRainyActivity : AppCompatActivity() {
-    private lateinit var mSurveyRainyModel: SurveyRainyViewModel
-    private val mSurveyRainyAdapter = SurveyRainyAdapter()
+class SurveyCloudyActivity : AppCompatActivity() {
+    private lateinit var mSurveyCloudyModel: SurveyCloudyViewModel
+    private val mSurveyCloudyAdapter = SurveyCloudyAdapter()
     private lateinit var getData: GetData
     val setData = SetData()
 
-    private var uid: Int? = null
-//    private var tid: Int? = null
-    private lateinit var email: String
-//    private lateinit var name: String
+    private var id: Int? = null
     private var tid_list = arrayListOf<Int?>(null, null, null, null, null, null, null)
     private var rating = arrayListOf<Int?>(null, null, null, null, null, null, null)
 
@@ -38,49 +35,48 @@ class SurveyRainyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_survey_rainy)
+        setContentView(R.layout.activity_survey_cloudy)
 
-        lsSurveyRainyResult.apply {
+        lsSurveyCloudyResult.apply {
             layoutManager = LinearLayoutManager(applicationContext)
             setHasFixedSize(true)
             itemAnimator = DefaultItemAnimator()
-            adapter = mSurveyRainyAdapter
+            adapter = mSurveyCloudyAdapter
         }
 
         if (requestQueue == null) {
             requestQueue = Volley.newRequestQueue(applicationContext)
         }
 
-        mSurveyRainyModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(application)).get(SurveyRainyViewModel::class.java)
+        mSurveyCloudyModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(application)).get(SurveyCloudyViewModel::class.java)
 
-        mSurveyRainyModel.survey_rainy_list.observe(this, Observer<ArrayList<SurveyRainyViewModel.SurveyRainy>>{
-            mSurveyRainyAdapter.notifyDataSetChanged()
+        mSurveyCloudyModel.survey_cloudy_list.observe(this, Observer<ArrayList<SurveyCloudyViewModel.SurveyCloudy>>{
+            mSurveyCloudyAdapter.notifyDataSetChanged()
         })
-        mSurveyRainyModel.requestSurveyRainy()
+        mSurveyCloudyModel.requestSurveyCloudy()
 
         getData = GetData()
 
-        email = intent.getStringExtra("email").toString()
+        val email = intent.getStringExtra("email").toString()
 
         for (i in 0 until getData.getUserSize()) {
             if (email == getData.getUser(i).email) {
-                uid = getData.getUser(i).id
+                id = getData.getUser(i).id
             }
         }
+        val uid = id?.let { intent.getIntExtra("uid", it) }
 
-        val btn_survey_rainy = findViewById<Button>(R.id.btn_survey_rainy)
-        btn_survey_rainy.setOnClickListener {
+        val btn_survey_cloudy = findViewById<Button>(R.id.btn_survey_cloudy)
+        btn_survey_cloudy.setOnClickListener {
 
             Log.i("d", "btn_tid_list$tid_list")
-            setData.postRate(uid, tid_list, 3, rating)
-            val sunny = Intent(this, SurveySunnyActivity::class.java)
-            sunny.putExtra("email", email)
-            sunny.putExtra("uid", uid)
-            startActivity(sunny)
+            setData.postRate(uid, tid_list, 2, rating)
+            val login = Intent(this, LogInActivity::class.java)
+            startActivity(login)
         }
     }
 
-    inner class SurveyRainyAdapter: RecyclerView.Adapter<SurveyRainyAdapter.ViewHolder>() {
+    inner class SurveyCloudyAdapter: RecyclerView.Adapter<SurveyCloudyAdapter.ViewHolder>() {
         inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)  {
             val txName: TextView
             val rgItem1: RadioButton
@@ -90,7 +86,7 @@ class SurveyRainyActivity : AppCompatActivity() {
             val rgItem5: RadioButton
 
             init {
-                txName = itemView.findViewById<TextView>(R.id.tx_survey_rainy_name)
+                txName = itemView.findViewById<TextView>(R.id.tx_survey_cloudy_name)
                 rgItem1 = itemView.findViewById<RadioButton>(R.id.radio_item1)
                 rgItem2 = itemView.findViewById<RadioButton>(R.id.radio_item2)
                 rgItem3 = itemView.findViewById<RadioButton>(R.id.radio_item3)
@@ -101,21 +97,21 @@ class SurveyRainyActivity : AppCompatActivity() {
         }
 
         override fun getItemCount(): Int {
-            return mSurveyRainyModel.getSize()
+            return mSurveyCloudyModel.getSize()
         }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SurveyRainyAdapter.ViewHolder {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SurveyCloudyAdapter.ViewHolder {
             val view = layoutInflater.inflate(
-                R.layout.item_survey_rainy,
+                R.layout.item_survey_cloudy,
                 parent,
                 false)
             return ViewHolder(view)
         }
 
-        override fun onBindViewHolder(holder: SurveyRainyAdapter.ViewHolder, position: Int) {
-            holder.txName.text = mSurveyRainyModel.getSurveyRainy(position).name
+        override fun onBindViewHolder(holder: SurveyCloudyAdapter.ViewHolder, position: Int) {
+            holder.txName.text = mSurveyCloudyModel.getSurveyCloudy(position).name
 
-            tid_list[position] = mSurveyRainyModel.getSurveyRainy(position).id
+            tid_list[position] = mSurveyCloudyModel.getSurveyCloudy(position).id
             Log.i("d", "tid_list$tid_list")
 
             holder.rgItem1.setOnClickListener {
@@ -144,7 +140,7 @@ class SurveyRainyActivity : AppCompatActivity() {
             }
         }
 
-        private fun setRadio(holder: SurveyRainyAdapter.ViewHolder, selection: Int) {
+        private fun setRadio(holder: SurveyCloudyAdapter.ViewHolder, selection: Int) {
             if (selection == 1) holder.rgItem1.isChecked = true
             if (selection == 2) holder.rgItem2.isChecked = true
             if (selection == 3) holder.rgItem3.isChecked = true
---
+++ app/src/main/java/kr/ac/kumoh/s/weatherable/SurveyCloudyViewModel.kt
@@ -0,0 +1,69 @@
+package kr.ac.kumoh.s.weatherable
+
+import android.app.Application
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonArrayRequest
+import org.json.JSONArray
+import org.json.JSONObject
+
+
+class SurveyCloudyViewModel(application: Application) : AndroidViewModel(application) {
+    companion object {
+        const val QUEUE_TAG = "VolleyRequest"
+        const val SERVER_URL = "https://flask-weatherable-wkrtj.run.goorm.io"
+        // "http://192.168.200.176:8080"
+        // http://192.168.0.11:8080
+    }
+
+    data class SurveyCloudy(var id: Int, var name: String)
+
+    private var mQueue: RequestQueue
+    val survey_cloudy_list = MutableLiveData<ArrayList<SurveyCloudy>>()
+    private val survey_cloudy_data = ArrayList<SurveyCloudy>()
+
+    init {
+        survey_cloudy_list.value = survey_cloudy_data
+        mQueue = MySingleton.getInstance(application).requestQueue
+        requestSurveyCloudy()
+    }
+
+    fun requestSurveyCloudy() {
+        val request = JsonArrayRequest(
+            Request.Method.GET,
+            "$SERVER_URL/tag_place", // SERVER_URL에 테이블 이름을 붙여준다.
+            null,
+            {
+                survey_cloudy_data.clear()
+                parseJson(it)
+                survey_cloudy_list.value = survey_cloudy_data
+            },
+            { Toast.makeText(getApplication(), it.toString(), Toast.LENGTH_LONG).show() }
+        )
+
+        request.tag = QUEUE_TAG
+        mQueue.add(request)
+    }
+
+    fun getSurveyCloudy(i: Int) = survey_cloudy_data[i]
+
+    fun getSize() = survey_cloudy_data.size
+
+    override fun onCleared() {
+        super.onCleared()
+        mQueue.cancelAll(QUEUE_TAG)
+    }
+
+    private fun parseJson(items: JSONArray) {
+        for(i in 0 until items.length()) {
+            val item: JSONObject = items[i] as JSONObject
+            val id = item.getInt("id")
+            val name = item.getString("title")
+
+            survey_cloudy_data.add(SurveyCloudy(id, name))
+        }
+    }
+}
